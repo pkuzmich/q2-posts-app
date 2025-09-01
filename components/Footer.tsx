3fb12112930b235ba3b0d9583881f1093760468c
Pavel Kuzmich <kuzmich@sovanet.cz>
2025-09-01 11:12:05 +0200

    Adding Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__container container">
+        <p>{new Date().getFullYear()} | Q2 Interactive s.r.o.</p>
+      </div>
+    </footer>
+  )
+}
