a1994d9a6ec15ff6af63ca5f974d594eb88d9f9d
Pavel Kuzmich <kuzmich@sovanet.cz>
2025-09-01 11:11:35 +0200

    Blog page update

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
-import Link from 'next/link'
 import Posts from '@/components/Posts'
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <section className="blog">
+      <div className="blog__container">
+        <div className="blog__header">
+          <h2 className="blog__title">Nejnovější</h2>
+        </div>
         <Posts />
-        <Link href="/posts">Příspěvky</Link>
-        <Link href="/posts/add">Přidat příspěvek</Link>
-      </main>
-    </div>
+      </div>
+    </section>
   )
 }
